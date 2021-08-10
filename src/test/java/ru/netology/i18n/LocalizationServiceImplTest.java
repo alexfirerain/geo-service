5d0b61c8f7d3f947da3e30bd67e309d4d58cfda4
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-10 19:37:56 +0300

    seemingly done

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -8,15 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LocalizationServiceImplTest {
     static final LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
-    static final String msgRus = "Добро пожаловать",
-                        msgEng = "Welcome";
+    static final String MSG_RUS = "Добро пожаловать",
+                        MSG_ENG = "Welcome";
 
     @ParameterizedTest
     @EnumSource(Country.class)
     void locally_greets(Country state) {
+
         if (Country.RUSSIA.equals(state))
-            assertEquals(msgRus, localizationService.locale(state));
+            assertEquals(MSG_RUS, localizationService.locale(state));
         else
-            assertEquals(msgEng, localizationService.locale(state));
+            assertEquals(MSG_ENG, localizationService.locale(state));
+
+        // или так:
+        assertEquals(state.equals(Country.RUSSIA) ?
+                        MSG_RUS : MSG_ENG,
+                localizationService.locale(state));
     }
 }
--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -2,22 +2,52 @@ package ru.netology.sender;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageSenderImplTest {
-    private static GeoService geoMock = Mockito.mock(GeoServiceImpl.class);
-
-
-    private static LocalizationServiceImpl localizationMock = Mockito.mock(LocalizationServiceImpl.class);
-
-    private static final MessageSenderImpl messageSender
-            = new MessageSenderImpl(geoMock, localizationMock);
+    static final String MSG_RUS = "Добро пожаловать", MSG_USA = "Welcome";
+    static Map<String, String> headers = new HashMap<>();
 
     @Test
-    void send() {
+    void when_172_sends_RUS() {
+        // IP "172.123.12.19" explicitly set to return Russian location:
+        GeoService geoMock = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geoMock.byIp("172.123.12.19")).thenReturn(new Location(null, Country.RUSSIA, null, 0));
+
+        // localization guaranteed to speak Russian when invoked from RUSSIA:
+        LocalizationServiceImpl localizationMock = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(localizationMock.locale(Country.RUSSIA)).thenReturn(MSG_RUS);
+
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoMock, localizationMock);
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+
+        assertEquals(MSG_RUS, messageSender.send(headers));
+    }
+
+    @Test
+    void when_96_sends_USA() {
+        // IP "96.123.12.19" explicitly set to return United States location:
+        GeoService geoMock = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geoMock.byIp("96.123.12.19")).thenReturn(new Location(null, Country.USA, null, 0));
+
+        // localization guaranteed to speak American when invoked from the USA:
+        LocalizationServiceImpl localizationMock = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(localizationMock.locale(Country.USA)).thenReturn(MSG_USA);
+
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoMock, localizationMock);
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.123.12.19");
+
+        assertEquals(MSG_USA, messageSender.send(headers));
     }
 }
